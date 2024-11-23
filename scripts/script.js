ed2bde8af6bc5935cc08c2f98f02e10aa1cebc38
Osama <osamarefaat93@gmail.com>
2024-11-23 17:52:11 +0200

    Did html layout

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-function changeTitle() {
-    let title = document.getElementById('osama');
-    title.innerHTML = 'Hello Osama';
-}
-
-let btn = document.getElementById('ti');
-btn.addEventListener('click' , changeTitle);
